b6b357409b3a07712e117ac17541be237fedbba1
mharris021 <mharris021@hotmail.com>
2017-12-22 16:18:36 -0600

    5 test phaser's create bodies from tilemap functionality.
    All Code and assets copied from project 4
    
    Next action is to clean up project 4 commented code

---
+++ 5-ph-3-test-creating-bodies-from-tilemap/src/PhaserGame.js
@@ -0,0 +1,23 @@
+import Phaser from 'phaser';
+import GameScene from "./GameScene"
+
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'phaser-test',
+    title: 'Testing React with Phaser v3',
+    version: '0.0.1',
+    scene: [
+        GameScene
+    ]
+
+};
+
+export default class PhaserGame extends Phaser.Game {
+
+    constructor() {
+        super(config);
+    }
+
+}
